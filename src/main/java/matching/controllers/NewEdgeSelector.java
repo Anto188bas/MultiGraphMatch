c831daa7adefda4bc0b549272324030cc597a18f
Roberto Grasso <robertograsso20@gmail.com>
2022-02-21 12:50:50 +0100

    Target Edges #6 - find candidates function adapted to the new target edges management;

--- src/main/java/matching/controllers/NewEdgeSelector.java
+++ src/main/java/matching/controllers/NewEdgeSelector.java
@@ -3,6 +3,7 @@ package matching.controllers;
 import cypher.models.QueryEdge;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import matching.models.MatchingData;
+import org.javatuples.Triplet;
 import state_machine.StateStructures;
 import target_graph.graph.GraphPaths;
 import tech.tablesaw.api.Row;
@@ -42,24 +43,23 @@ public class NewEdgeSelector {
 
     // 1A. TYPE VECTOR IS EMPTY
     public static void no_types_case(
-          ArrayList<Stream<Row>> candidates,
+          ArrayList<Triplet<Integer, Integer, Integer>> candidates,
           MatchingData     matchingData,
           IntArrayList[]   nodes_symmetry,
           IntArrayList     listCandidates,
           int              q_node,
-          String[]         cols_name,
+          int[]         cols,
           GraphPaths       graphPaths
     ) {
         // NOTE: cols_name has size 1 if the query is directed, otherwise 2. it contains the column name
         //       related to target nodes list that have to be matched.
-        for(int i = 0; i < cols_name.length; i++){
-            String column_name = cols_name[i];
-            candidates.get(i).forEach(row -> {
-            //for (Row row : candidates.get(i)) {
-                int t_node = row.getInt(column_name);
+        for(int i = 0; i < cols.length; i++){
+            int column = cols[i];
+            candidates.forEach(triplet -> {
+                int t_node = (column == 0 ? triplet.getValue0() : triplet.getValue1());
                 if (!matchingData.matchedNodes.contains(t_node) &&
                     nodeCondCheck(q_node, t_node, matchingData, nodes_symmetry)){
-                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[row.getInt(2)];
+                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[triplet.getValue2()];
                     for (IntArrayList edges: colors_edges) {
                         if (edges == null) continue;
                         for(int idEdge: edges) {
@@ -76,52 +76,47 @@ public class NewEdgeSelector {
 
     // 1B. TYPE VECTOR IS EMPTY (FIRST NODE)
     public static void no_type_case(
-         ArrayList<Stream<Row>> candidates,
-         int[]            query_nodes,
+         ArrayList<Triplet<Integer, Integer, Integer>> candidates,
+         int q_node,
          GraphPaths       graphPaths,
          MatchingData     matchingData,
          IntArrayList[]   nodes_symmetry,
          IntArrayList     listCandidates
     ){
-        for(int i = 0; i < query_nodes.length; i++) {
-            final int ii = i;
-            candidates.get(i).forEach(row->{
-            //for (Row row : candidates.get(i)) {
-                int t_node = row.getInt(0);
-                if (nodeCondCheck(query_nodes[ii], t_node, matchingData, nodes_symmetry)){
-                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[row.getInt(2)];
-                    for (IntArrayList edges: colors_edges) {
-                        if (edges == null) continue;
-                        for(int idEdge: edges) {
-                            listCandidates.add(idEdge);
-                            listCandidates.add(t_node);
-                            listCandidates.add(row.getInt(1));
-                        }
+        candidates.forEach(triple -> {
+            int t_node = triple.getValue0();
+            if (nodeCondCheck(q_node, t_node, matchingData, nodes_symmetry)){
+                IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[triple.getValue2()];
+                for (IntArrayList edges: colors_edges) {
+                    if (edges == null) continue;
+                    for(int idEdge: edges) {
+                        listCandidates.add(idEdge);
+                        listCandidates.add(t_node);
+                        listCandidates.add(triple.getValue1().intValue());
                     }
                 }
-            });
-        }
+            }
+        });
     }
 
     // 2A. TYPE VECTOR IS SET
     public static void types_case(
-           ArrayList<Stream<Row>> candidates,
+           ArrayList<Triplet<Integer, Integer, Integer>> candidates,
            MatchingData     matchingData,
            IntArrayList[]   nodes_symmetry,
            IntArrayList     listCandidates,
            int              q_node,
-           String[]         cols_name,
+           int[]         cols,
            GraphPaths       graphPaths,
            QueryEdge        query_edge
     ) {
-        for(int i = 0; i < cols_name.length; i++){
-            String column_name = cols_name[i];
-            candidates.get(i).forEach(row -> {
-            //for (Row row : candidates.get(i)) {
-                int t_node = row.getInt(column_name);
+        for(int i = 0; i < cols.length; i++){
+            int column = cols[i];
+            candidates.forEach(triplet -> {
+                int t_node = (column == 0 ? triplet.getValue0() : triplet.getValue1());
                 if (!matchingData.matchedNodes.contains(t_node) &&
                     nodeCondCheck(q_node, t_node, matchingData, nodes_symmetry)){
-                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[row.getInt(2)];
+                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[triplet.getValue2()];
                     for(int color: query_edge.getEdge_label()) {
                         IntArrayList edges = colors_edges[color];
                         if (edges == null) continue;
@@ -138,8 +133,8 @@ public class NewEdgeSelector {
 
     // 2B. TYPE VECTOR IS SET (FIRST NODE)
     public static void types_case(
-            ArrayList<Stream<Row>> candidates,
-            int[]            query_nodes,
+            ArrayList<Triplet<Integer, Integer, Integer>> candidates,
+            int query_node,
             GraphPaths       graphPaths,
             MatchingData     matchingData,
             IntArrayList[]   nodes_symmetry,
@@ -148,30 +143,27 @@ public class NewEdgeSelector {
             int              t_src,
             int              t_dst
     ){
-        for(int i = 0; i < query_nodes.length; i++) {
-            if (nodeCondCheck(query_nodes[i], t_src, matchingData, nodes_symmetry)){
-                candidates.get(i).forEach(row -> {
-                //for (Row row : candidates.get(i)) {
-                    int key = row.getInt(2);
-                    IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[key];
-                    for(int color: query_edge.getEdge_label()) {
-                        IntArrayList edges = colors_edges[color];
-                        if (edges == null) continue;
-                        for(int idEdge: edges) {
-                            listCandidates.add(idEdge);
-                            listCandidates.add(t_src);
-                            listCandidates.add(t_dst);
-                        }
+        if (nodeCondCheck(query_node, t_src, matchingData, nodes_symmetry)){
+            candidates.forEach(triplet -> {
+                int key = triplet.getValue2();
+                IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[key];
+                for(int color: query_edge.getEdge_label()) {
+                    IntArrayList edges = colors_edges[color];
+                    if (edges == null) continue;
+                    for(int idEdge: edges) {
+                        listCandidates.add(idEdge);
+                        listCandidates.add(t_src);
+                        listCandidates.add(t_dst);
                     }
-                });
-            }
+                }
+            });
         }
     }
 
 
     // 3A. BOTH MATCHED AND TYPE VECTOR IS UNSET
     public static void no_types_case_matched_nodes(
-            ArrayList<Stream<Row>> candidates,
+            ArrayList<Triplet<Integer, Integer, Integer>> candidates,
             MatchingData     matchingData,
             IntArrayList[]   edges_symmetry,
             IntArrayList     listCandidates,
@@ -179,24 +171,22 @@ public class NewEdgeSelector {
             StateStructures  states,
             int              q_edge
     ) {
-        for (Stream<Row> candidate: candidates)
-            candidate.forEach(row -> {
-            //for (Row row : candidate) {
-                IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[row.getInt(2)];
-                for (IntArrayList edges : colors_edges) {
-                    if (edges == null) continue;
-                    for (int idEdge : edges) {
-                        if (!matchingData.matchedEdges.contains(idEdge) &&
-                                condCheckEdges(q_edge, idEdge, matchingData, edges_symmetry, states))
-                            listCandidates.add(idEdge);
-                    }
+        candidates.forEach(triplet -> {
+            IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[triplet.getValue2()];
+            for (IntArrayList edges : colors_edges) {
+                if (edges == null) continue;
+                for (int idEdge : edges) {
+                    if (!matchingData.matchedEdges.contains(idEdge) &&
+                            condCheckEdges(q_edge, idEdge, matchingData, edges_symmetry, states))
+                        listCandidates.add(idEdge);
                 }
-            });
+            }
+        });
     }
 
     // 4A. BOTH MATCHED AND TYPE VECTOR IS UNSET
     public static void type_case_matched_nodes(
-            ArrayList<Stream<Row>> candidates,
+            ArrayList<Triplet<Integer, Integer, Integer>> candidates,
             MatchingData     matchingData,
             IntArrayList[]   edges_symmetry,
             IntArrayList     listCandidates,
@@ -205,20 +195,17 @@ public class NewEdgeSelector {
             int              q_edge,
             QueryEdge        query_edge
     ){
-        for (Stream<Row> candidate: candidates) {
-            candidate.forEach(row -> {
-            //for (Row row : candidate) {
-                IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[row.getInt(2)];
-                for (int color : query_edge.getEdge_label()) {
-                    IntArrayList edges = colors_edges[color];
-                    if (edges == null) continue;
-                    for (int idEdge : edges) {
-                        if (!matchingData.matchedEdges.contains(idEdge) &&
-                            condCheckEdges(q_edge, idEdge, matchingData, edges_symmetry, states))
-                            listCandidates.add(idEdge);
-                    }
+        candidates.forEach(triplet -> {
+            IntArrayList[] colors_edges = graphPaths.getMap_key_to_edge_list()[triplet.getValue2()];
+            for (int color : query_edge.getEdge_label()) {
+                IntArrayList edges = colors_edges[color];
+                if (edges == null) continue;
+                for (int idEdge : edges) {
+                    if (!matchingData.matchedEdges.contains(idEdge) &&
+                        condCheckEdges(q_edge, idEdge, matchingData, edges_symmetry, states))
+                        listCandidates.add(idEdge);
                 }
-            });
-        }
+            }
+        });
     }
 }
--- src/main/java/matching/controllers/NewFindCandidates.java
+++ src/main/java/matching/controllers/NewFindCandidates.java
@@ -6,6 +6,8 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 import matching.models.MatchingData;
 import ordering.EdgeDirection;
 import ordering.NodesPair;
+import org.javatuples.Triplet;
+import org.opencypher.v9_0.expressions.In;
 import state_machine.StateStructures;
 import target_graph.graph.GraphPaths;
 import tech.tablesaw.api.Row;
@@ -16,10 +18,10 @@ import java.util.stream.Stream;
 
 public class NewFindCandidates {
     // GLOBAL VARIABLE
-    private static final String[] src_vet = new String[] {"src"};
-    private static final String[] dst_vet = new String[] {"dst"};
-    private static final String[] src_dst = new String[] {"src", "dst"};
-    private static final String[] dst_src = new String[] {"dst", "src"};
+    private static final int[] src_vet = new int[] {0};
+    private static final int[] dst_vet = new int[] {1};
+    private static final int[] src_dst = new int[] {0, 1};
+    private static final int[] dst_src = new int[] {1, 0};
 
 
     // FIND CANDIDATE FOR SELECTED EDGES (FROM STATE 1 TO STATE N)
@@ -34,90 +36,105 @@ public class NewFindCandidates {
     ) {
 //        // PARAMETERS
         IntArrayList listCandidates    = new IntArrayList();
-//        int           edge_id           = states.map_state_to_edge[sel_state];
-//        NodesPair     edge_data         = query.getMap_edge_to_endpoints().get(edge_id);
-//        QueryEdge     queryEdge         = query.getQuery_edge(edge_id);
-//        EdgeDirection direction         = states.map_edge_to_direction[edge_id];
-//        IntArrayList  edge_type         = queryEdge.getEdge_label();
-//
-//        // QUERY NODES
-//        int q_src                      = edge_data.getFirstEndpoint();
-//        int q_dst                      = edge_data.getSecondEndpoint();
-//        // TARGET NODES
-//        int t_src                      = matchingData.solution_nodes[q_src];
-//        int t_dst                      = matchingData.solution_nodes[q_dst];
-//
-//        ArrayList<Stream<Row>> edges_submap  = new ArrayList<>();
-//        String[]         cols_name;
-//        // A. t_dst IS MATCHED
-//        if(t_src == -1) {
-//           Table compatible_subtable   = edge_data.getBySecondValue(t_dst);
-//
-//           // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
-//           if (direction == EdgeDirection.OUT) {          // src to match
-//               edges_submap.add(graphPaths.getByDSTandSRCs(t_dst, compatible_subtable.intColumn("first")));
-//               cols_name = src_vet;
-//           }
-//           // 2. EDGE FROM q_dst TO q_src (INBOUND)
-//           else if (direction == EdgeDirection.IN) {    // dst to match
-//               edges_submap.add(graphPaths.getBySRCandDSTs(t_dst, compatible_subtable.intColumn("first")));
-//               cols_name = dst_vet;
-//           }
-//           // 3. UNDIRECTED CASE          (BOTH)
-//           else {                        // 1. src to match and 2. dst to match
-//               edges_submap.add(graphPaths.getByDSTandSRCs(t_dst, compatible_subtable.intColumn("first")));
-//               edges_submap.add(graphPaths.getBySRCandDSTs(t_dst, compatible_subtable.intColumn("first")));
-//               cols_name = src_dst;
-//           }
-//           // CANDIDATE CONFIGURATION
-//           if (edge_type.size() == 0)
-//               NewEdgeSelector.no_types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_src, cols_name, graphPaths);
-//           else
-//               NewEdgeSelector.types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_src, cols_name, graphPaths, queryEdge);
-//        }
-//
-//        // B. t_src IS MATCHED
-//        else if (t_dst == -1){
-//            Table compatible_subtable   = edge_data.getByFirstValue(t_src);
-//            // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
-//            if (direction == EdgeDirection.OUT) {
-//                edges_submap.add(graphPaths.getBySRCandDSTs(t_src, compatible_subtable.intColumn("second")));
-//                cols_name = dst_vet;
-//            }
-//            // 2. EDGE FROM q_dst TO q_src (INBOUND)
-//            else if (direction == EdgeDirection.IN) {
-//                edges_submap.add(graphPaths.getByDSTandSRCs(t_src, compatible_subtable.intColumn("second")));
-//                cols_name = src_vet;
-//            }
-//            // 3. UNDIRECTED CASE          (BOTH)
-//            else {
-//                edges_submap.add(graphPaths.getBySRCandDSTs(t_src, compatible_subtable.intColumn("second")));
-//                edges_submap.add(graphPaths.getByDSTandSRCs(t_src, compatible_subtable.intColumn("second")));
-//                cols_name = dst_src;
-//            }
-//            // CANDIDATE CONFIGURATION
-//            if (edge_type.size() == 0)
-//                NewEdgeSelector.no_types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_dst, cols_name, graphPaths);
-//            else
-//                NewEdgeSelector.types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_dst, cols_name, graphPaths, queryEdge);
-//        }
-//        else {
-//            // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
-//            if (direction == EdgeDirection.OUT)
-//                edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
-//            // 2. EDGE FROM q_dst TO q_src (INBOUND)
-//            else if (direction == EdgeDirection.IN)
-//                edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
-//            // 3. UNDIRECTED CASE          (BOTH)
-//            else {
-//                edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
-//                edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
-//            }
-//            if (edge_type.size() == 0)
-//                NewEdgeSelector.no_types_case_matched_nodes(edges_submap, matchingData, edges_symmetry, listCandidates, graphPaths, states, edge_id);
-//            else
-//                NewEdgeSelector.type_case_matched_nodes(edges_submap, matchingData, edges_symmetry, listCandidates, graphPaths, states, edge_id, queryEdge);
-//        }
+        int           edge_id           = states.map_state_to_edge[sel_state];
+        NodesPair     edge_data         = query.getMap_edge_to_endpoints().get(edge_id);
+        QueryEdge     queryEdge         = query.getQuery_edge(edge_id);
+        EdgeDirection direction         = states.map_edge_to_direction[edge_id];
+        IntArrayList  edge_type         = queryEdge.getEdge_label();
+
+        // QUERY NODES
+        int q_src                      = edge_data.getFirstEndpoint();
+        int q_dst                      = edge_data.getSecondEndpoint();
+        // TARGET NODES
+        int t_src                      = matchingData.solution_nodes[q_src];
+        int t_dst                      = matchingData.solution_nodes[q_dst];
+
+        ArrayList<Triplet<Integer, Integer, Integer>> edges_submap  = new ArrayList<>();
+        int[]         cols;
+        // A. t_dst IS MATCHED
+        if(t_src == -1) {
+           Table compatible_subtable   = edge_data.getBySecondValue(t_dst);
+
+           // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
+           if (direction == EdgeDirection.OUT) {          // src to match
+               edges_submap.addAll(graphPaths.getByDSTandSRCs(t_dst, compatible_subtable.intColumn("first")));
+               cols = src_vet;
+           }
+           // 2. EDGE FROM q_dst TO q_src (INBOUND)
+           else if (direction == EdgeDirection.IN) {    // dst to match
+               edges_submap.addAll(graphPaths.getBySRCandDSTs(t_dst, compatible_subtable.intColumn("first")));
+               cols = dst_vet;
+           }
+           // 3. UNDIRECTED CASE          (BOTH)
+           else {                        // 1. src to match and 2. dst to match
+               edges_submap.addAll(graphPaths.getByDSTandSRCs(t_dst, compatible_subtable.intColumn("first")));
+               edges_submap.addAll(graphPaths.getBySRCandDSTs(t_dst, compatible_subtable.intColumn("first")));
+               cols = src_dst;
+           }
+           // CANDIDATE CONFIGURATION
+           if (edge_type.size() == 0)
+               NewEdgeSelector.no_types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_src, cols, graphPaths);
+           else
+               NewEdgeSelector.types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_src, cols, graphPaths, queryEdge);
+        }
+
+        // B. t_src IS MATCHED
+        else if (t_dst == -1){
+            Table compatible_subtable   = edge_data.getByFirstValue(t_src);
+            // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
+            if (direction == EdgeDirection.OUT) {
+                edges_submap.addAll(graphPaths.getBySRCandDSTs(t_src, compatible_subtable.intColumn("second")));
+                cols = dst_vet;
+            }
+            // 2. EDGE FROM q_dst TO q_src (INBOUND)
+            else if (direction == EdgeDirection.IN) {
+                edges_submap.addAll(graphPaths.getByDSTandSRCs(t_src, compatible_subtable.intColumn("second")));
+                cols = src_vet;
+            }
+            // 3. UNDIRECTED CASE          (BOTH)
+            else {
+                edges_submap.addAll(graphPaths.getBySRCandDSTs(t_src, compatible_subtable.intColumn("second")));
+                edges_submap.addAll(graphPaths.getByDSTandSRCs(t_src, compatible_subtable.intColumn("second")));
+                cols = dst_src;
+            }
+            // CANDIDATE CONFIGURATION
+            if (edge_type.size() == 0)
+                NewEdgeSelector.no_types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_dst, cols, graphPaths);
+            else
+                NewEdgeSelector.types_case(edges_submap, matchingData, nodes_symmetry, listCandidates, q_dst, cols, graphPaths, queryEdge);
+        }
+        else {
+            // 1. EDGE FROM q_src TO q_dst (OUTBOUND)
+            if (direction == EdgeDirection.OUT) {
+                Triplet<Integer, Integer, Integer> triplet = graphPaths.getBySRCandDST(t_src, t_dst);
+
+                if(triplet != null) {
+                    edges_submap.add(triplet);
+                }
+            }
+            // 2. EDGE FROM q_dst TO q_src (INBOUND)
+            else if (direction == EdgeDirection.IN) {
+                Triplet<Integer, Integer, Integer> triplet = graphPaths.getBySRCandDST(t_dst, t_src);
+                if(triplet != null) {
+                    edges_submap.add(triplet);
+                }
+            }
+            // 3. UNDIRECTED CASE          (BOTH)
+            else {
+                Triplet<Integer, Integer, Integer> triplet = graphPaths.getBySRCandDST(t_src, t_dst);
+                if(triplet != null) {
+                    edges_submap.add(triplet);
+                }
+                triplet = graphPaths.getBySRCandDST(t_dst, t_src);
+                if(triplet != null) {
+                    edges_submap.add(triplet);
+                }
+            }
+            if (edge_type.size() == 0)
+                NewEdgeSelector.no_types_case_matched_nodes(edges_submap, matchingData, edges_symmetry, listCandidates, graphPaths, states, edge_id);
+            else
+                NewEdgeSelector.type_case_matched_nodes(edges_submap, matchingData, edges_symmetry, listCandidates, graphPaths, states, edge_id, queryEdge);
+        }
         return listCandidates;
     }
 
@@ -132,31 +149,28 @@ public class NewFindCandidates {
         StateStructures states
     ){
         IntArrayList listCandidates    = new IntArrayList();
-//        QueryEdge        queryEdge     = query.getQuery_edge(edge_id);
-//        EdgeDirection    direction     = states.map_edge_to_direction[edge_id];
-//        IntArrayList     edge_type     = queryEdge.getEdge_label();
-//        ArrayList<Stream<Row>> edges_submap  = new ArrayList<>();
-//        int[]            q_node;
-//
-//        // q_src = t_src AND q_dst = t_dst
-//        if (direction == EdgeDirection.OUT) {
-//            edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
-//            q_node = new int[] {q_src};
-//        }
-//        else if (direction == EdgeDirection.IN) {
-//            edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
-//            q_node = new int[] {q_src};
-//        }
-//        else {
-//            edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
-//            edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
-//            q_node = new int[] {q_src, q_src};
-//        }
-//
-//        if (edge_type.size() == 0)
-//            NewEdgeSelector.no_type_case(edges_submap, q_node, graphPaths, matchingData, nodes_symmetry, listCandidates);
-//        else
-//            NewEdgeSelector.types_case(edges_submap, q_node, graphPaths, matchingData, nodes_symmetry, listCandidates, queryEdge, t_src, t_dst);
+        QueryEdge        queryEdge     = query.getQuery_edge(edge_id);
+        EdgeDirection    direction     = states.map_edge_to_direction[edge_id];
+        IntArrayList     edge_type     = queryEdge.getEdge_label();
+        ArrayList<Triplet<Integer, Integer, Integer>> edges_submap  = new ArrayList<>();
+        int q_node = q_src;
+
+        // q_src = t_src AND q_dst = t_dst
+        if (direction == EdgeDirection.OUT) {
+            edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
+        }
+        else if (direction == EdgeDirection.IN) {
+            edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
+        }
+        else {
+            edges_submap.add(graphPaths.getBySRCandDST(t_src, t_dst));
+            edges_submap.add(graphPaths.getBySRCandDST(t_dst, t_src));
+        }
+
+        if (edge_type.size() == 0)
+            NewEdgeSelector.no_type_case(edges_submap, q_node, graphPaths, matchingData, nodes_symmetry, listCandidates);
+        else
+            NewEdgeSelector.types_case(edges_submap, q_node, graphPaths, matchingData, nodes_symmetry, listCandidates, queryEdge, t_src, t_dst);
         return listCandidates;
     }
 
