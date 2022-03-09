a214d242bfc6ed7e28ff76799e7632026da8c354
rosariocannavo <rosariocannavo2000@gmail.com>
2022-03-09 16:07:06 +0100

    Created the graph using hashmaps:    - implemented a graph reading the node hashmap    - mapped the labels    - TODO: create the edges using the hashmap

--- src/test/java/algorithms_test.java
+++ src/test/java/algorithms_test.java
@@ -1,10 +1,22 @@
 import algorithms.*;
 import com.google.gson.Gson;
+import configuration.Configuration;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import reading.FileManager;
+import target_graph.edges.EdgeHandler;
+import target_graph.graph.GraphPaths;
+import target_graph.nodes.GraphMacroNode;
+import target_graph.nodes.MacroNodeHandler;
+import target_graph.propeties_idx.NodesEdgesLabelsMaps;
+import tech.tablesaw.api.Table;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class algorithms_test {
     public static void main(String[] args) throws IOException {
@@ -166,12 +178,65 @@ public class algorithms_test {
         writer.close();
 
 
-        //Label Hashmap testing
-        VertexLabelMap vertexMap = new VertexLabelMap();
-        vertexMap.setVertexLabel(0, "a");
-        for (Integer iterator : directedGraph.vertexSet()) {
-            System.out.println("label of vertex: " + iterator + ":" + vertexMap.getVertexLabel(iterator));
-        }
+
+
+        // CONFIGURATION
+        Configuration configuration     = new Configuration(args);
+        NodesEdgesLabelsMaps idx_label  = new NodesEdgesLabelsMaps();
+
+        // TARGET READING
+        Table[] nodes_tables            = FileManager.files_reading(configuration.nodes_main_directory, ',');
+        Table[] edges_tables_properties = FileManager.files_reading(configuration.edges_main_directory, ',');
+
+
+        // NODE ELABORATION
+        HashMap<String, GraphMacroNode> macro_nodes  = new HashMap<>();
+        Int2ObjectOpenHashMap<String> nodes_macro  = new Int2ObjectOpenHashMap<>();
+        Int2ObjectOpenHashMap<ArrayList<String>> level_nodeId = new Int2ObjectOpenHashMap<>();
+        int max_deep_level = MacroNodeHandler.graph_macro_node_creation(
+                nodes_tables,"type", idx_label, macro_nodes, level_nodeId, nodes_macro
+        );
+
+        // EDGE ELABORATION
+        Int2ObjectOpenHashMap<Int2ObjectOpenHashMap<IntOpenHashSet[]>> src_dst_aggregation = new Int2ObjectOpenHashMap<>();
+        GraphPaths graphPaths = EdgeHandler.createGraphPaths(edges_tables_properties, idx_label, src_dst_aggregation);
+
+
+
+        //create a test graph reading from the hashmap
+        Graph<Integer, RelationshipEdge> testGraph = new SimpleDirectedGraph<>(RelationshipEdge.class);
+        for(int i = 0; i<nodes_macro.size(); i++)
+            testGraph.addVertex(i);
+
+
+        //id = id_vertex -> get -> label
+        testGraph.vertexSet().stream()
+                             .forEach(i -> System.out.println(i+": "+nodes_macro.get(i)));
+
+
+        testGraph.addEdge(0, 1, new RelationshipEdge("FRIEND"));
+        testGraph.addEdge(1, 3, new RelationshipEdge("RELATIVE"));
+
+
+        ShortestPath s2= new ShortestPath(testGraph);
+        System.out.println(s2.DijkstraSP(0,3));
+
+
+
+       // System.out.println(nodes_macro.get(0));  //label of vertex 0
+       // System.out.println(nodes_macro.get(1));
+
+
+        var map = src_dst_aggregation.get(5673);
+        System.out.println(src_dst_aggregation.get(5673).int2ObjectEntrySet());  //key -> src = ritorna la lista delle destination
+
+
+        // TODO read edge and edge labels
+
+      //  System.out.println(Arrays.toString(map.get(7029)));
+      //  System.out.println(Arrays.toString(map.get(9997)));
+      //  System.out.println(Arrays.toString(map.get(6496)));
+
 
     }
 }
