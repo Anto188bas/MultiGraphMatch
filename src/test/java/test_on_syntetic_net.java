097d06519acde779c1f147f6beadf58bbc6ba327
Anto188bas <antoniodm_2@msn.com>
2022-03-31 14:31:59 +0200

    Commit: 2022-03-31    - timeout implementation (it does not work)

--- src/test/java/test_on_syntetic_net.java
+++ src/test/java/test_on_syntetic_net.java
@@ -6,7 +6,6 @@ import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import matching.controllers.NewMatching;
 import matching.models.OutData;
 import reading.FileManager;
-
 import target_graph.edges.EdgeHandler;
 import target_graph.graph.GraphPaths;
 import target_graph.nodes.GraphMacroNode;
@@ -14,7 +13,9 @@ import target_graph.nodes.MacroNodeHandler;
 import target_graph.propeties_idx.NodesEdgesLabelsMaps;
 import tech.tablesaw.api.Table;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.*;
+import java.util.concurrent.*;
 
 
 public class test_on_syntetic_net {
@@ -32,7 +33,7 @@ public class test_on_syntetic_net {
         Int2ObjectOpenHashMap<String>            nodes_macro  = new Int2ObjectOpenHashMap<>();
         Int2ObjectOpenHashMap<ArrayList<String>> level_nodeId = new Int2ObjectOpenHashMap<>();
         int max_deep_level = MacroNodeHandler.graph_macro_node_creation(
-                nodes_tables,"type", idx_label, macro_nodes, level_nodeId, nodes_macro
+            nodes_tables,"type", idx_label, macro_nodes, level_nodeId, nodes_macro
         );
 
         // EDGE ELABORATION
@@ -47,23 +48,41 @@ public class test_on_syntetic_net {
 
         // QUERIES READING
         List<String> queries = FileManager.query_reading(configuration);
+        final Duration tout  = Duration.ofSeconds(configuration.timeout);
         queries.forEach(query -> {
-            QueryStructure query_obj = new QueryStructure();
-            query_obj.parser(query, idx_label);
+            System.out.println(query);
+            ExecutorService exec = Executors.newSingleThreadExecutor();
+            final Future<Double> handler = exec.submit(new Callable<Double>() {
+                @Override
+                public Double call() throws Exception {
+                    QueryStructure query_obj = new QueryStructure();
+                    query_obj.parser(query, idx_label);
 
-            // MATCHING
-            OutData outData = NewMatching.matching(
-                    true, false, Long.MAX_VALUE, idx_label, target_bitmatrix,
-                    query_obj, graphPaths, macro_nodes, nodes_macro
-            );
+                    // MATCHING
+                    OutData outData = NewMatching.matching(
+                        true, false, Long.MAX_VALUE, idx_label, target_bitmatrix,
+                        query_obj, graphPaths, macro_nodes, nodes_macro
+                    );
 
-            if(configuration.out_file != null) {
-                try {
-                    FileManager.saveIntoCSV(query, configuration.out_file, outData);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    // SAVING
+                    if(configuration.out_file != null) {
+                        try {
+                            FileManager.saveIntoCSV(query, configuration.out_file, outData);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return outData.domain_time + outData.matching_time + outData.ordering_time + outData.symmetry_time;
                 }
+            });
+            try {handler.get(tout.getSeconds(), TimeUnit.SECONDS);}
+            catch (Exception e) {
+                handler.cancel(true);
+                e.printStackTrace();
+                System.err.println("timeout");
             }
+            exec.shutdownNow();
+
         });
 
     }
